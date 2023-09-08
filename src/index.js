fcb37b8ef89ca1e5d8d82c6a4ca176acdb74461e
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-09-08 15:53:36 -0500

    Implement fetching weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello, world');
+const model = () => {
+  // Secure this in prod
+  const apiKey = "8dfac318e5d043b1af2192352230809";
+  const baseURL = "https://api.weatherapi.com/v1";
+
+  // Define paramaters
+  const params = new URLSearchParams({
+    key: apiKey,
+    q: "auto:ip",
+    days: 3,
+  });
+
+  // Construct the request URL
+  const fullURL = `${baseURL}/forecast.json?${params.toString()}`;
+
+  // Define the async function
+  async function getWeather() {
+    // First, fetch the data
+    const response = await fetch(fullURL, { mode: "cors" });
+    // Then return the JSON
+    return response.json();
+  }
+  // Do the fetching
+  getWeather().then((data) => {
+    console.log(data);
+  });
+};
+model();
